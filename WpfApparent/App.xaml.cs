288135ed5a9165a51a3251e3809efd11240abc70
zy-zhao17 <zy-zhao17@mails.tsinghua.edu.cn>
2021-04-15 17:46:19 +0800

    first commit

---
+++ WpfApparent/App.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+using Application = System.Windows.Application;
+using MessageBox = System.Windows.MessageBox;
+
+namespace WpfApparent
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private static NotifyIcon trayIcon;
+
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            RemoveTrayIcon();
+            AddTrayIcon();
+        }
+
+        private void AddTrayIcon()
+        {
+            if (trayIcon != null)
+            {
+                return;
+            }
+            trayIcon = new NotifyIcon
+            {
+                Icon = System.Drawing.SystemIcons.WinLogo,
+                Text = "NotifyIconStd"
+            };
+            trayIcon.Visible = true;
+
+            ContextMenu menu = new ContextMenu();
+
+            MenuItem refreshItem = new MenuItem();
+            refreshItem.Text = "Refresh";
+            refreshItem.Click += refreshItem_Click;
+
+
+            MenuItem closeItem = new MenuItem();
+            closeItem.Text = "Close";
+            closeItem.Click += new EventHandler(delegate { this.Shutdown(); });
+
+            MenuItem helpItem = new MenuItem();
+            helpItem.Text = "Help";
+            helpItem.Click += helpItem_Click;
+
+            menu.MenuItems.Add(refreshItem);
+            menu.MenuItems.Add(closeItem);
+            menu.MenuItems.Add(helpItem);
+
+            trayIcon.ContextMenu = menu;    //设置NotifyIcon的右键弹出菜单
+        }
+
+        private void refreshItem_Click(object sender, EventArgs e)
+        {
+            MainWindow.Close();
+            new MainWindow().Show();
+        }
+
+
+        private void helpItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("F3：降低亮度\nF4：提高亮度\nAlt+R/G/B/T/H/N：增加/减少叠加在屏幕上的颜色\n注意：本程序不能额外提高屏幕亮度！", "帮助");
+        }
+
+
+        private void RemoveTrayIcon()
+        {
+            if (trayIcon != null)
+            {
+                trayIcon.Visible = false;
+                trayIcon.Dispose();
+                trayIcon = null;
+            }
+        }
+
+        private void ApplicationExit(object sender, ExitEventArgs e)
+        {
+            RemoveTrayIcon();
+        }
+    }
+}
---
+++ WpfApparent/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+
+
+namespace WpfApparent
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr window, int index);
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr window, int index, int value);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+
+
+        [DllImport("User32.Dll")]
+        public static extern void SetWindowText(int h, String s);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool RegisterHotKey(
+            IntPtr hWnd,
+            int id,
+            KeyModifiers fsModifiers,
+            int vk
+            );
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool UnregisterHotKey(
+            IntPtr hWnd,
+            int id
+            );
+
+        [DllImport("kernel32", SetLastError = true)]
+        public static extern short GlobalAddAtom(string lpString);
+
+        [DllImport("kernel32", SetLastError = true)]
+        public static extern short GlobalDeleteAtom(short nAtom);
+
+        [Flags()]
+        public enum KeyModifiers
+        {
+            None = 0,
+            Alt = 1,
+            Ctrl = 2,
+            Shift = 4,
+            WindowsKey = 8
+        }
+        public const int WmHotkey = 0x312;
+        readonly Dictionary<string, short> hotKeyDic = new Dictionary<string, short>();
+
+
+        public static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);    //窗体置顶
+        public static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);    //取消窗体置顶
+        public const uint SWP_NOMOVE = 0x0002;    //不调整窗体位置
+        public const uint SWP_NOSIZE = 0x0001;    //不调整窗体大小
+        public const int GWL_EXSTYLE = -20;
+        public const int WS_EX_TRANSPARENT = 0x00000020;
+        public IntPtr hwnd;
+        byte liangdu = 65;
+        byte r = 0;
+        byte g = 0;
+        byte b = 0;
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            hwnd = new WindowInteropHelper(this).Handle;
+            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT | 3);
+
+
+            var wpfHwnd = new WindowInteropHelper(this).Handle;
+            var hWndSource = HwndSource.FromHwnd(wpfHwnd);
+            if (hWndSource != null) hWndSource.AddHook(MainWindowProc);
+            hotKeyDic.Add("F3", GlobalAddAtom("F3"));
+            hotKeyDic.Add("F4", GlobalAddAtom("F4"));
+            hotKeyDic.Add("R+", GlobalAddAtom("R+"));
+            hotKeyDic.Add("R-", GlobalAddAtom("R-"));
+            hotKeyDic.Add("G+", GlobalAddAtom("G+"));
+            hotKeyDic.Add("G-", GlobalAddAtom("G-"));
+            hotKeyDic.Add("B+", GlobalAddAtom("B+"));
+            hotKeyDic.Add("B-", GlobalAddAtom("B-"));
+            RegisterHotKey(wpfHwnd, hotKeyDic["F3"], KeyModifiers.None, (int)Keys.F3);
+            RegisterHotKey(wpfHwnd, hotKeyDic["F4"], KeyModifiers.None, (int)Keys.F4);
+            RegisterHotKey(wpfHwnd, hotKeyDic["R+"], KeyModifiers.Alt, (int)Keys.R);
+            RegisterHotKey(wpfHwnd, hotKeyDic["R-"], KeyModifiers.Alt, (int)Keys.T);
+            RegisterHotKey(wpfHwnd, hotKeyDic["G+"], KeyModifiers.Alt, (int)Keys.G);
+            RegisterHotKey(wpfHwnd, hotKeyDic["G-"], KeyModifiers.Alt, (int)Keys.H);
+            RegisterHotKey(wpfHwnd, hotKeyDic["B+"], KeyModifiers.Alt, (int)Keys.B);
+            RegisterHotKey(wpfHwnd, hotKeyDic["B-"], KeyModifiers.Alt, (int)Keys.N);
+        }
+
+
+        private IntPtr MainWindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WmHotkey:
+                    {
+                        int sid = wParam.ToInt32();
+
+                        if (sid == hotKeyDic["F3"])
+                        {
+
+                            if (liangdu <= 250) liangdu += 5;
+                            Background = new SolidColorBrush(Color.FromArgb(liangdu, r, g, b));
+                        }
+                        else if (sid == hotKeyDic["F4"])
+                        {
+                            if (liangdu >= 5) liangdu -= 5;
+                            Background = new SolidColorBrush(Color.FromArgb(liangdu, r, g, b));
+                        }
+                        else if (sid == hotKeyDic["R-"])
+                        {
+                            if (r >= 5) r -= 5;
+                            Background = new SolidColorBrush(Color.FromArgb(liangdu, r, g, b));
+                        }
+                        else if (sid == hotKeyDic["R+"])
+                        {
+                            if (r <= 250) r += 5;
+                            Background = new SolidColorBrush(Color.FromArgb(liangdu, r, g, b));
+                        }
+                        else if (sid == hotKeyDic["G-"])
+                        {
+                            if (g >= 5) g -= 5;
+                            Background = new SolidColorBrush(Color.FromArgb(liangdu, r, g, b));
+                        }
+                        else if (sid == hotKeyDic["G+"])
+                        {
+                            if (g <= 250) g += 5;
+                            Background = new SolidColorBrush(Color.FromArgb(liangdu, r, g, b));
+                        }
+                        else if (sid == hotKeyDic["B-"])
+                        {
+                            if (b >= 5) b -= 5;
+                            Background = new SolidColorBrush(Color.FromArgb(liangdu, r, g, b));
+                        }
+                        else if (sid == hotKeyDic["B+"])
+                        {
+                            if (b <= 250) b += 5;
+                            Background = new SolidColorBrush(Color.FromArgb(liangdu, r, g, b));
+                        }
+                        handled = true;
+                        break;
+                    }
+            }
+
+            return IntPtr.Zero;
+        }
+
+    }
+}
